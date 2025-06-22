c5734e0b91533e4f951afb0e47e2bca313a3cd8c
frgiovanna <giovannacnf@gmail.com>
2025-06-22 15:02:29 -0300

    feat: add fastify server

---
+++ server.js
@@ -0,0 +1,21 @@
+const Fastify = require('fastify');
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server running...');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
